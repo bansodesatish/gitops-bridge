fbf5f636d90f46d670f181feb19563768d2c81d6
satish.bansode <satish.m.bansode@gmail.com>
2024-04-02 13:09:29 +0530

    update aws secret manager configuration

--- argocd/iac/terraform/examples/eks/multi-cluster/distributed/main.tf
+++ argocd/iac/terraform/examples/eks/multi-cluster/distributed/main.tf
@@ -4,6 +4,8 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 data "aws_availability_zones" "available" {}
 
+provider "bcrypt" {}
+
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
@@ -148,6 +150,42 @@ module "gitops_bridge_bootstrap" {
     addons       = local.addons
   }
   apps = local.argocd_apps
+  # This example shows how to set default ArgoCD Admin Password using SecretsManager with Helm Chart set_sensitive values.
+  argocd = {
+    set_sensitive = [
+      {
+        name  = "configs.secret.argocdServerAdminPassword"
+        value = bcrypt_hash.argo.id
+      }
+    ]
+  }
+}
+
+################################################################################
+# ArgoCD Admin Password credentials with Secrets Manager
+# Login to AWS Secrets manager with the same role as Terraform to extract the ArgoCD admin password with the secret name as "argocd"
+################################################################################
+resource "random_password" "argocd" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+# Argo requires the password to be bcrypt, we use custom provider of bcrypt,
+# as the default bcrypt function generates diff for each terraform plan
+resource "bcrypt_hash" "argo" {
+  cleartext = random_password.argocd.result
+}
+
+#tfsec:ignore:aws-ssm-secret-use-customer-key
+resource "aws_secretsmanager_secret" "argocd" {
+  name                    = module.eks.cluster_name
+  recovery_window_in_days = 0 # Set to zero for this example to force delete during Terraform destroy
+}
+
+resource "aws_secretsmanager_secret_version" "argocd" {
+  secret_id     = aws_secretsmanager_secret.argocd.id
+  secret_string = random_password.argocd.result
 }
 
 ################################################################################
--- argocd/iac/terraform/examples/eks/multi-cluster/distributed/outputs.tf
+++ argocd/iac/terraform/examples/eks/multi-cluster/distributed/outputs.tf
@@ -13,9 +13,9 @@ output "configure_argocd" {
     aws eks --region ${local.region} update-kubeconfig --name ${module.eks.cluster_name}
     export ARGOCD_OPTS="--port-forward --port-forward-namespace argocd --grpc-web"
     kubectl config set-context --current --namespace argocd
-    argocd login --port-forward --username admin --password $(argocd admin initial-password | head -1)
+    argocd login --port-forward --username admin --password $(aws secretsmanager get-secret-value --secret-id argocd --output json | jq -r .SecretString)
     echo "ArgoCD Username: admin"
-    echo "ArgoCD Password: $(kubectl get secrets argocd-initial-admin-secret -n argocd --template="{{index .data.password | base64decode}}")"
+    echo "ArgoCD Password: $(aws secretsmanager get-secret-value --secret-id ${module.eks.cluster_name} --output json | jq -r .SecretString)"
     echo Port Forward: http://localhost:8080
     kubectl port-forward -n argocd svc/argo-cd-argocd-server 8080:80
     EOT
@@ -26,8 +26,8 @@ output "access_argocd" {
   value       = <<-EOT
     export KUBECONFIG="/tmp/${module.eks.cluster_name}"
     aws eks --region ${local.region} update-kubeconfig --name ${module.eks.cluster_name}
-    echo "ArgoCD Username: admin"
-    echo "ArgoCD Password: $(kubectl get secrets argocd-initial-admin-secret -n argocd --template="{{index .data.password | base64decode}}")"
     echo "ArgoCD URL: https://$(kubectl get svc -n argocd argo-cd-argocd-server -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')"
+    echo "ArgoCD Username: admin"
+    echo "ArgoCD Password: $(aws secretsmanager get-secret-value --secret-id ${module.eks.cluster_name} --output json | jq -r .SecretString)"
     EOT
 }
